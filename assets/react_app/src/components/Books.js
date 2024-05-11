06a9c56a6bd4177dff5ce3abcabb0bcce9d87944
kerrigan17 <celiatr.17@gmail.com>
2024-05-12 00:00:44 +0200

    cambios

--- assets/react_app/src/components/Books.js
+++ assets/react_app/src/components/Books.js
@@ -1,29 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import './Books.css';
+import './Books.css'; 
 
 function Books() {
     const [books, setBooks] = useState([]);
 
     useEffect(() => {
         fetch('/books')
-        .then(response => response.json())
-        .then(data => setBooks(data.data))
-        .catch(error => console.error('Error fetching data:', error));
+            .then(response => response.json())
+            .then(data => setBooks(data.data))
+            .catch(error => console.error('Error fetching data:', error));
     }, []);
 
     return (
         <div className="books-container">
-        <h1>Libros Disponibles</h1>
-        <div className="book-list">
-            {books.map(book => (
-            <div key={book.id} className="book-item">
-                <h2>{book.title}</h2>
-                <p>ISBN: {book.isbn}</p>
-                <p>Año de Publicación: {book.publicationYear}</p>
-                <p>Categoría: {book.category}</p>
+            <h1>Libros Disponibles</h1>
+            <div className="book-list">
+                {books.map(book => (
+                    <div key={book.id} className="book-item">
+                        <h2>{book.title}</h2>
+                        <p>ISBN: {book.isbn}</p>
+                        <p>Año de Publicación: {book.publicationYear}</p>
+                        <p>Categoría: {book.category}</p>
+                    </div>
+                ))}
             </div>
-            ))}
-        </div>
         </div>
     );
 }
